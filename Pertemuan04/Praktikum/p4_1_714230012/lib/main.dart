4a5007451726ed3af5fd91066318562feecb4fe9
Muflih Afif <muflihafif234@gmail.com>
2025-10-29 12:44:01 +0700

    Push ptaktikum pertemeuan04

---
+++ Pertemuan04/Praktikum/p4_1_714230012/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tempat Wisata Bandung',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                'Ranca Upas',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                fontSize:  30,
+                fontWeight: FontWeight.bold,
+              ),),
+            ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: const <Widget>[
+                    Icon(Icons.calendar_today),
+                    SizedBox(height: 8.0,),
+                    Text('Open Everyday'),
+                  ],
+                ),
+                Column(
+                  children: const <Widget>[
+                    Icon(Icons.access_time),
+                    SizedBox(height: 8.0,),
+                    Text('09:00 - 20:00'),
+                  ],
+                ),
+                Column(
+                  children: const <Widget>[
+                    Icon(Icons.attach_money),
+                    SizedBox(height: 8.0,),
+                    Text('Rp 20.000'),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: const Text(
+              'Ranca Upas adalah kawasan bumi perkemahan di bawah pengelolaan perhutani. Temapat ini berada di kawasan wisata Bandung Selatan, satu lokasi dengan kawah putih , kolam Cimanggu dan situ Patenggang. Banyak hal yang bisa dilakukan di kawasan wisata ini, seperti berkemah, berinteraksi dengan rusa, sampai bermain di water park dan mandi air panas.',
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+          )
+          ],
+        ),
+      ),
+    );
+  }
+}
